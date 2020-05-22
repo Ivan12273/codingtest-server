f2988cef4a4d68afe4c976d17e99fff367ee91e3
Ivan12273 <cas12273@gmail.com>
2020-05-22 11:50:48 -0500

    Ruta create problem

--- routes/classroom.js
+++ routes/classroom.js
@@ -8,5 +8,6 @@ var router = express.Router();
 router.get('/get-all-results', ClassroomController.getAllResults);
 router.get('/problem/:student/:organization/:assignment', ClassroomController.problem);
 router.get('/calification/:organization/:assignment/:student/:problemAlias/:language/:executionTime', ClassroomController.calification);
+router.post('/create-problem', ClassroomController.createProblem);
 
 module.exports = router;
